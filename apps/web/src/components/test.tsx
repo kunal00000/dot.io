4c8221c8d0c115fb1df1c8cde7d4dcf60c7288bb
kunal00000 <kunalverma2468@gmail.com>
2023-11-12 04:11:35 +0530

    chore: trying out the procedures

--- apps/web/src/components/test.tsx
+++ apps/web/src/components/test.tsx
@@ -3,8 +3,33 @@
 import { trpc } from '@/app/_trpc/client';
 
 const Test = () => {
-  const x = trpc.gethello.useQuery();
-  return <div>{JSON.stringify(x.data)}</div>;
+  const y = trpc.getShortUrl.useQuery(
+    {
+      originalUrl: 'https://google.com'
+    },
+    {
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: false
+    }
+  );
+
+  const x = trpc.shorten.useMutation({
+    onSettled: () => {
+      console.log('refetch');
+      y.refetch();
+    }
+  });
+
+  return (
+    <div>
+      {y.data?.shortUrl}
+      <br />
+      <button onClick={() => x.mutate({ originalUrl: 'https://google.com' })}>
+        hell
+      </button>
+    </div>
+  );
 };
 
 export default Test;
