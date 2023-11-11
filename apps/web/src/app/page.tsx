dd927ffb34c703ffb19c90a93b53246ee666faa7
kunal00000 <kunalverma2468@gmail.com>
2023-11-12 04:11:15 +0530

    chore: testing procedures

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,20 @@
 import Test from '@/components/test';
 import Image from 'next/image';
-import { trpc } from './_trpc/client';
 import { serverClient } from './_trpc/serverClient';
 
 export default async function Home() {
+  /**
+   * fetched on server (no flickering on reload)
+   * ISSUE: if link not found in db, it will return 404 trpc error
+   */
+  // const s = await serverClient.getShortUrl({
+  //   originalUrl: 'https://google.com'
+  // });
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <Test />
+      {/* {s.shortUrl} */}
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
         <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
           Get started by editing&nbsp;
