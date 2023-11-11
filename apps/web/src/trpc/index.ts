4f936d598ece050a8cf428e91a31c2112b19d36d
kunal00000 <kunalverma2468@gmail.com>
2023-11-12 04:05:02 +0530

    feat: get short url and shortening procedures defined

--- apps/web/src/trpc/index.ts
+++ apps/web/src/trpc/index.ts
@@ -1,23 +1,70 @@
 import { z } from 'zod';
 import { publicProcedure, router } from './trpc';
+import { nanoid } from 'nanoid';
+import { db } from '@/db';
+import { TRPCError } from '@trpc/server';
 
 export const appRouter = router({
-  hello: publicProcedure
-    .input(
-      z.object({
-        text: z.string()
-      })
-    )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`
-      };
-    }),
-  gethello: publicProcedure.query((opts) => {
+  hello: publicProcedure.query(() => {
     return {
       greeting: `hello`
     };
-  })
+  }),
+
+  // this is mutate procedure for url shortening
+  getShortUrl: publicProcedure
+    .input(
+      z.object({
+        originalUrl: z.string().url()
+      })
+    )
+    .query(async ({ input }) => {
+      const { originalUrl } = input;
+
+      const linkFound = await db.link.findFirst({
+        where: {
+          targetUrl: originalUrl
+        }
+      });
+
+      if (!linkFound) throw new TRPCError({ code: 'NOT_FOUND' });
+
+      return {
+        shortUrl: linkFound.shortUrl
+      };
+    }),
+
+  shorten: publicProcedure
+    .input(
+      z.object({
+        originalUrl: z.string().url()
+      })
+    )
+    .mutation(async ({ input }) => {
+      const { originalUrl } = input;
+
+      const linkExist = await db.link.findFirst({
+        where: {
+          targetUrl: originalUrl
+        }
+      });
+      if (linkExist)
+        throw new TRPCError({
+          code: 'CONFLICT',
+          message: 'Link already exists'
+        });
+
+      const shortKey = nanoid(8);
+
+      await db.link.create({
+        data: {
+          shortUrl: shortKey,
+          targetUrl: originalUrl
+        }
+      });
+
+      return { success: true };
+    })
 });
 
 export type AppRouter = typeof appRouter;
